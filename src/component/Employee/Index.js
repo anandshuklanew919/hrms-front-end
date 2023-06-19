7f1ccf92956aaf3b76eecdb7673d92b3a7b64a41
Anand Shukla <anandshuklanew919@gmail.com>
2023-06-19 13:00:56 +0530

    employee sidebar menu added

--- src/component/Employee/Index.js
+++ src/component/Employee/Index.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Grid } from '@mui/material'
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../../UI/Sidebar/Sidebar'
+
+
 
 const EMPSIDEMENU = [
-    { menupath: 'company', menutext: 'Company', icon: 'BusinessIcon' },
-    { menupath: 'businessunit', menutext: 'Business Unit', icon: 'DomainAddIcon' },
-    { menupath: 'department', menutext: 'Department', icon: 'GroupIcon' },
-    { menupath: 'section', menutext: 'Section', icon: 'ClassIcon' }
+    { menupath: '.', menutext: 'Employee Dashboard', icon: 'DashboardIcon' },
+    { menupath: 'summary', menutext: 'Summary', icon: 'SummarizeIcon' },
+    { menupath: 'new', menutext: 'New Employee', icon: 'AddCardIcon' },
 ]
 
 
 
 export default function EmployeeRoot() {
     return (
-        <Grid item xs={12}>
-            <Outlet />
-        </Grid>
+        <>
+            <Sidebar sidemenus={EMPSIDEMENU} />
+            <Grid item xs={12} sx={{ position: 'absolute', top: '75px', left: '125px' }}>
+                <Outlet />
+            </Grid >
+        </>
     )
+
 }
--- src/component/Masters/Index.js
+++ src/component/Masters/Index.js
@@ -4,9 +4,8 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from '../../UI/Sidebar/Sidebar'
 
 
-const EMPSIDEMENU = [
-    { menupath: '.', menutext: 'Master Dashboard', icon: 'DashboardIcon' },
-    { menupath: 'company', menutext: 'Company', icon: 'BusinessIcon' },
+const MASTERSIDEMENU = [
+    { menupath: '.', menutext: 'Employee Dashboard', icon: 'DashboardIcon' },
     { menupath: 'businessunit', menutext: 'Business Unit', icon: 'DomainAddIcon' },
     { menupath: 'department', menutext: 'Department', icon: 'GroupIcon' },
     { menupath: 'section', menutext: 'Section', icon: 'ClassIcon' }
@@ -15,7 +14,7 @@ const EMPSIDEMENU = [
 export default function MasterRoot() {
     return (
         <>
-            <Sidebar sidemenus={EMPSIDEMENU} />
+            <Sidebar sidemenus={MASTERSIDEMENU} />
             <Grid item xs={12} sx={{ position: 'absolute', top: '75px', left: '125px' }}>
                 <Outlet />
             </Grid >
